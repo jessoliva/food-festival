29b5dde89ff69ef4da5249919d299e5a4b384967
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-04-29 22:03:42 -0500

    committing before separation of concerns

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const path = require("path");
 // require because plugin is built into webpack, and need to ensure we're req webpack's methods and properties into this file  
 const webpack = require("webpack");
 
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 // For a basic configuration, we need to provide webpack with three properties: entry, output, and mode
 module.exports = {
 
@@ -21,6 +23,10 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+        // we added the BundleAnalyzerPlugin, we configured the analyzerMode property with a static value. This will output an HTML file called report.html that will generate in the dist folder.
     ],
     mode: 'development'
 };
